f0fce77f9183caf4b5137ce058f34d86d15f130c
Mariia_Grebeshkova <138971111+mmariiaregiina@users.noreply.github.com>
2023-10-01 19:30:34 +0300

    Update Aim_constraint_head.py

--- Aim_constraint_head.py
+++ Aim_constraint_head.py
@@ -9,13 +9,11 @@ selection = cmds.ls(sl=1)
 if len(selection) == 0:
     cmds.inViewMessage(amg='ERROR: <hl>you have to select an object</hl>', pos='midCenter', fade=1)
 
-position = cmds.xform(selection, q=1, t=1, ws=1)
-x = position [0]
-y = position [1]
-z = position [2]
-
+# Creating locators in a pivot using Match transform:
 loc_up = cmds.spaceLocator(n='LOC_UP') 
 loc_front = cmds.spaceLocator(n='LOC_FRONT')
+cmds.matchTransform(loc_up, selection, pos=1, rot=1, scl=1)
+cmds.matchTransform(loc_front, selection, pos=1, rot=1, scl=1)
 
 cmds.color(loc_up, rgb=(149, 0, 179))
 cmds.color(loc_front, rgb=(149, 0, 179))
@@ -28,10 +26,12 @@ cmds.setAttr ("LOC_UPShape.localScaleX", 12)
 cmds.setAttr ("LOC_UPShape.localScaleY", 12)
 cmds.setAttr ("LOC_UPShape.localScaleZ", 12)
 
-cmds.xform(loc_up, t=(x, y+50, z))
-cmds.xform(loc_front, t=(x, y, z+40))
+# Moving locators:
+cmds.move(0, 50, 0, loc_up, wd=1, r=1, os=1)
+cmds.move(0, 0, 40, loc_front, wd=1, r=1, os=1)
 
-cmds.parentConstraint(selection,  loc_up, mo=1)
+# Constrain aim:
+cmds.parentConstraint(selection, loc_up, mo=1)
 cmds.parentConstraint(selection, loc_front, mo=1)
 
 minTime = cmds.playbackOptions(query=1, minTime=1)
@@ -45,7 +45,8 @@ cmds.delete("LOC_FRONT_parentConstraint1")
 
 cmds.aimConstraint(loc_front, selection, worldUpType='object', worldUpObject="LOC_UP", aimVector=[0, 0, 1], upVector=[0, 1, 0])
 
-cmds.group( loc_up, loc_front, n="my_sys_for_head")
+# Put in group and freeze:
+cmds.group(loc_up, loc_front, n="my_sys_for_head")
 cmds.setAttr("my_sys_for_head.tx", lock=1)
 cmds.setAttr("my_sys_for_head.ty", lock=1)
 cmds.setAttr("my_sys_for_head.tz", lock=1)
